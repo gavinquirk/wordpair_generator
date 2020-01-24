dcd2a0ec2a7f48d9ccb1e9a66411dc3419eb347d
Gavin Quirk <gavinquirk@gmail.com>
2020-01-24 14:56:08 -0800

    toggle saved word, icon

--- lib/random_words.dart
+++ lib/random_words.dart
@@ -8,6 +8,7 @@ class RandomWords extends StatefulWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final _randomWordPairs = <WordPair>[];
+  final _savedWordPairs = Set<WordPair>();
 
   Widget _buildList() {
     return ListView.builder(
@@ -27,8 +28,20 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _savedWordPairs.contains(pair);
     return ListTile(
-        title: Text(pair.asPascalCase, style: TextStyle(fontSize: 18)));
+        title: Text(pair.asPascalCase, style: TextStyle(fontSize: 18)),
+        trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
+            color: alreadySaved ? Colors.red : null),
+        onTap: () {
+          setState(() {
+            if (alreadySaved) {
+              _savedWordPairs.remove(pair);
+            } else {
+              _savedWordPairs.add(pair);
+            }
+          });
+        });
   }
 
   Widget build(BuildContext context) {
