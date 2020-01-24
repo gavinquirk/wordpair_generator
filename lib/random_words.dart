a437e7ad56475b610dbb8ed24f600f3471ae73bf
Gavin Quirk <gavinquirk@gmail.com>
2020-01-24 15:06:27 -0800

    saved wordpair page

--- lib/random_words.dart
+++ lib/random_words.dart
@@ -44,8 +44,30 @@ class RandomWordsState extends State<RandomWords> {
         });
   }
 
+  void _pushSaved() {
+    Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext) {
+      final Iterable<ListTile> tiles = _savedWordPairs.map((WordPair pair) {
+        return ListTile(
+            title: Text(pair.asPascalCase, style: TextStyle(fontSize: 16)));
+      });
+
+      final List<Widget> divided =
+          ListTile.divideTiles(context: context, tiles: tiles).toList();
+
+      return Scaffold(
+          appBar: AppBar(title: Text('Saved WordPairs')),
+          body: ListView(children: divided));
+    }));
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('WordPair Generator')), body: _buildList());
+        appBar: AppBar(
+          title: Text('WordPair Generator'),
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)
+          ],
+        ),
+        body: _buildList());
   }
 }
